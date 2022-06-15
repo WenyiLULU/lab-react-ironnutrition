d5b2191b65d5017c85b2d463cccdb5865ed63fa4
WenyiLULU <luwenyi1991@gmail.com>
2022-06-15 14:14:44 +0200

    Iteration 3 done

--- src/App.js
+++ src/App.js
@@ -3,34 +3,35 @@ import { Card, Row, Col, Divider, Input, Button } from 'antd';
 import basicFoods from './foods.json';
 import { useState } from 'react';
 import useId from 'react-use-uuid';
+import FoodBox from './Component/FoodBox';
+
+function addId(foodsArray){
+  const copiedFoodsArray = JSON.parse(JSON.stringify(foodsArray))
+  copiedFoodsArray.forEach(food => {
+    food.id = useId()
+  });
+  return copiedFoodsArray
+}
 
 function App() {
-  const [foods, setFoods] = useState(basicFoods)
+  const newArray = addId(basicFoods)  
+  //console.log(newArray)
+  const [foods, setFoods] = useState(newArray)
+  
   return (
     <div className="App">
-      <Row style={{ width: '100%', justifyContent: 'center' }}>
+      <Divider>Food List</Divider>
+
+      <Row style={{ width: '100%', justifyContent: 'center' }}>        
         {foods.map((food)=>{
-          
-          return(
-          <Col>
-            <Card
-              title={food.name}
-              style={{ width: 230, height: 300, margin: 10 }}
-            >
-              <img src={food.image} height={60} alt="food" />
-              <p>Calories: {food.calories}</p>
-              <p>Servings: {food.servings}</p>
-              <p>
-                <b>Total Calories: {food.calories * food.servings} </b> kcal
-              </p>
-              <Button type="primary"> Delete </Button>
-            </Card>
-          </Col>)
-        })}
-      </Row>
-      
+          return (
+            <Col key={food.id}>
+              <FoodBox food={food} />
+            </Col>
+          )           
+        })}        
+      </Row>      
     </div>
-  )
-    
+  )    
 }
 export default App;
